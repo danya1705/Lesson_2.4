17cb077170842fec7bd7baf0f553fef72f8301d5
danya1705 <danya17@gmail.com>
2022-09-14 12:00:48 +0300

    homework is refactored

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,77 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
 
         checkAuthorizationParameters("java_skypro.go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        System.out.println();
+        checkAuthorizationParameters("java_skypro_go", "D_1hWiKjjP-9", "D_1hWiKjjP_9");
+        System.out.println();
         checkAuthorizationParameters("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        System.out.println();
         checkAuthorizationParameters("01234567890123456789", "01234567890123456789", "01234567890123456789");
 
     }
 
     public static boolean checkAuthorizationParameters(String login, String password, String confirmPassword) {
 
+        boolean loginCorrect;
+        boolean passwordCorrect;
+
         try {
-            checkLogin(login);
-            checkPassword(password, confirmPassword);
+            loginCorrect = checkLogin(login);
+            passwordCorrect = checkPassword(password, confirmPassword);
         } catch (WrongLoginException e) {
-            System.out.println("Incorrect login");
+            System.out.println("Incorrect login length");
             return false;
         } catch (WrongPasswordException e) {
-            System.out.println("Incorrect password or password confirmation");
+            System.out.println("Password confirmation failed");
             return false;
         }
 
-        System.out.println("Authorization is OK");
-        return true;
+        if (loginCorrect && passwordCorrect) {
+            System.out.println("Authorization is OK");
+            return true;
+        }
+
+        return false;
     }
 
-    public static void checkLogin(String login) {
+    public static boolean checkLogin(String login) {
+
+        Pattern pattern = Pattern.compile("[a-zA-Z0-9_]+");
+        Matcher matcher = pattern.matcher(login);
+        if (!matcher.matches()) {
+            System.out.println("Incorrect login characters");
+            return false;
+        }
 
         if (login.length() > 20) throw new WrongLoginException();
 
-        for (int i = 0; i < login.length(); i++) {
-            if (!checkChar(login.charAt(i))) throw new WrongLoginException();
-        }
+        return true;
 
     }
 
-    public static void checkPassword(String password, String confirmPassword) {
+    public static boolean checkPassword(String password, String confirmPassword) {
 
-        if (password.length() >= 20 || !password.equals(confirmPassword)) {
+        if (password.length() >= 20) {
+            System.out.println("Incorrect password length");
+            return false;
+        }
+
+        if (!password.equals(confirmPassword)) {
             throw new WrongPasswordException();
         }
 
-        for (int i = 0; i < password.length(); i++) {
-            if (!checkChar(password.charAt(i))) throw new WrongPasswordException();
+        Pattern pattern = Pattern.compile("[a-zA-Z0-9_]+");
+        Matcher matcher = pattern.matcher(password);
+        if (!matcher.matches()) {
+            System.out.println("Incorrect password characters");
+            return false;
         }
 
+        return true;
     }
 
-    public static boolean checkChar(char ch) {
-        return (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_';
-    }
 }
